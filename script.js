f066d9f43ab4a04ceb3f8d5d1dbd5875a69c2fb8
SimonBesendorfer <simon.besendorfer@me.com>
2020-10-25 18:24:08 +0100

    working on burger btn

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.getElementById('burger-btn');
+let burgerBtn = document.getElementById('burger');
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
+
+function open(){
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuOpen = false;
+  }
+
+}
+menuBtn.addEventListener('click', function() {
   if(!menuOpen) {
     menuBtn.classList.add('open');
     menuOpen = true;
