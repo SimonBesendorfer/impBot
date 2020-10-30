598f5c11e24306cc6a154c49a67320ce2e142084
SimonBesendorfer <simon.besendorfer@me.com>
2020-10-30 11:53:01 +0100

    fixed shuffle again when matching

--- personal.js
+++ personal.js
@@ -2,7 +2,7 @@ let participants = [];
 let shuffled = [];
 let counter = 0;
 
-function addParticipant(){
+function addParticipant() {
     let participant = document.getElementById('participant').value;
     participants.push(participant);
     shuffled.push(participant);
@@ -14,8 +14,8 @@ function addParticipant(){
     checkForNext();
 }
 
-function checkForNext(){
-    if (counter >= 3){
+function checkForNext() {
+    if (counter >= 3) {
         document.getElementById('nextBtn').classList.remove('d-none');
     }
 }
@@ -24,31 +24,42 @@ function checkForNext(){
 // Fucntion to shuffle the array content 
 function shuffle() {
 
-    for (let i = shuffled.length - 1; i > 0; i--) { 
+    for (let i = shuffled.length - 1; i > 0; i--) {
         // Generate random number  
-        let j = Math.floor(Math.random() * (i + 1)); 
+        let j = Math.floor(Math.random() * (i + 1));
 
-        let temp = shuffled[i]; 
-        shuffled[i] = shuffled[j]; 
-        shuffled[j] = temp; 
-    } 
+        let temp = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = temp;
+    }
     console.log(shuffled);
     checkForConflict();
-} 
+}
 
 // Checks if the shuffled array mataches on any place with the previous array position
 function checkForConflict() {
-    for (let i = 0; i < shuffled.length; i++){
-        if(shuffled[i] == participants[i]){
-            shuffle();
-        } else {
-            getImp();
+    let conflict = false;
+    for (let i = 0; i < shuffled.length; i++) {
+        if (shuffled[i] == participants[i]) {
+            conflict = true;
         }
+    }
+    console.log(conflict);
+    if (conflict === true) {
+        shuffle();
+    } else {
+        getImp();
     }
 }
 
-function getImp(){
-    console.log("right way");
-    document.getElementById('content').innerHTML = "";
-    
+function getImp() {
+    document.getElementById('content').innerHTML = '';
+    let HTML = `<h2>
+    Unser Wichtel hat den Topf geschüttelt!
+    </h2>
+    <p>Gib das Handy an</p>
+    <h2>${participants[0]}</h2>
+    <p>weiter!</p>
+    <button id="nextBtn" href="#" class="myButton" style="margin-top: 12px" onclick="">weiter</button>`;
+    document.getElementById('content').insertAdjacentHTML("beforeend", HTML);
 }
